61a93c8ffe9688691e91c4f9830c38116cd7a2cf
Jira Vinyoopongphan <jira.vinyoo@gmail.com>
2018-12-20 15:04:59 +0200

    Create input form and render options
    Co-authored-by: Andrew Deal <deal.e.andrew@gmail.com>

--- src/app/components/OptionsList/index.tsx
+++ src/app/components/OptionsList/index.tsx
@@ -11,17 +11,52 @@
 
 import * as React from 'react';
 import Input from '@material-ui/core/Input';
-import { withStyles } from '@material-ui/core';
+import { withStyles, Divider } from '@material-ui/core';
+import Chip from '@material-ui/core/Chip';
 
 export default class OptionsList extends React.Component<any, any> {
-    constructor(props) {
+    constructor (props) {
         super(props);
         this.state = {
-            options: []
+            options: [],
+            current: ''
         };
     }
 
+    handleInput = (event) => {
+        this.setState({ current: event.target.value });
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        this.setState((state) => ({ options: state.options.concat([state.current]), current: '' }));
+    }
+
+    handleDelete = (value) => {
+        const newOptionsList = this.state.options.filter(item => item !== value);
+        this.setState({options: newOptionsList});
+    }
+
     public render() {
-        return (<Input value='testing'></Input>);
+        const { options } = this.state;
+        return (
+            <div>
+                {options.length > 0 ? options.map((item, index) => (
+                    <Chip
+                    label={item}
+                    onDelete={() => this.handleDelete(item)}
+                    />)) : 'Add an option'}
+                <form onSubmit={this.handleSubmit}>
+                    <Input type='text'
+                        placeholder='testing'
+                        value={this.state.current}
+                        onChange={this.handleInput} />
+                    <Input
+                        id='formatted-option-input'
+                        type='submit'
+                    />
+                </form>
+            </div>
+        );
     }
 }
