d3b685528d2ca44e3ea4cd3c4f6de05a63db7c8f
Andrew Deal <deal.e.andrew@gmail.com>
2019-01-03 18:46:17 +0200

    Updates form component with styles and input
    Co-authored-by: Jira Vinyoo <jira.vinyoo@gmail.com>

--- src/app/components/OptionsForm/index.tsx
+++ src/app/components/OptionsForm/index.tsx
@@ -1,12 +1,27 @@
 import * as React from 'react';
-import Chip from '@material-ui/core/Chip';
-import Fab from '@material-ui/core/Fab';
-import Input from '@material-ui/core/Input';
-import Modal from '@material-ui/core/Modal';
-import Typography from '@material-ui/core/Typography';
-import { withStyles, Divider } from '@material-ui/core';
+import {
+  createStyles,
+  withStyles,
+  Divider,
+  Fab,
+  Grid,
+  Input,
+  Modal,
+  Typography
+} from '@material-ui/core';
+import AddIcon from '@material-ui/icons/Add';
+import { OptionsGridComponent } from './OptionsGrid';
 
-import OptionsGrid from './OptionsGrid';
+const styles = createStyles({
+  formRow: {
+    margin: '24px 0px'
+  },
+  addFab: {
+    marginLeft: '10px',
+    height: '40px',
+    width: '40px'
+  }
+});
 
 interface OptionsFormState {
     options: string[][];
@@ -15,11 +30,11 @@ interface OptionsFormState {
     modalOpen: boolean;
 }
 
-export default class OptionsForm extends React.Component<any, any> {
+class OptionsForm extends React.Component<any, any> {
     constructor (props) {
         super(props);
         const state: OptionsFormState = {
-            options: [['one', 'two', 'three'], ['four', 'five', 'six'], ['seven', 'eight', 'nine']],
+            options: [[]],
             current: '',
             selected: '',
             modalOpen: false
@@ -34,7 +49,16 @@ export default class OptionsForm extends React.Component<any, any> {
 
     handleSubmit = (event) => {
         event.preventDefault();
-        this.setState((state) => ({ options: state.options.concat([state.current]), current: '' }));
+        const { options } = this.state;
+        const { current: optionToAdd } = this.state;
+        let lastOptionsRow = options[options.length - 1];
+
+        if (lastOptionsRow.length < 3) {
+          lastOptionsRow = lastOptionsRow.push(optionToAdd);
+        } else {
+          options.push([optionToAdd]);
+        }
+        this.setState(() => ({ options, current: '' }));
     }
 
     handleDelete = (value: string): void => {
@@ -51,38 +75,44 @@ export default class OptionsForm extends React.Component<any, any> {
 
     public render() {
         const { options } = this.state;
+        const { classes } = this.props;
         return (
-            <div>
+            <Grid container
+              justify='space-evenly'>
+              <Grid item
+                className={classes.formRow}>
                 <form onSubmit={this.handleSubmit}>
                     <Input type='text'
-                        placeholder='testing'
+                        placeholder='Add an option here...'
                         value={this.state.current}
-                        onChange={this.handleInput} />
-                    <Input
-                        id='formatted-option-input'
-                        type='submit'
-                    />
+                        onChange={this.handleInput}/>
+                    <Fab className={classes.addFab}
+                      type='submit'>
+                      <AddIcon />
+                    </Fab>
                 </form>
-                <OptionsGrid deleteHandler={this.handleDelete} options={this.state.options}></OptionsGrid>
-                <div>
-
-                    <Fab
-                        disabled={options.length < 2 ? true : false}
-                        color='primary'
-                        variant='extended'
-                        onClick={this.handleGenerate}
-                    >
-                        Generate
+              </Grid>
+              <OptionsGridComponent deleteHandler={this.handleDelete} options={this.state.options} />
+              <Grid item
+                className={classes.formRow}>
+                <Fab
+                    disabled={options.length < 2 ? true : false}
+                    color='primary'
+                    variant='extended'
+                    onClick={this.handleGenerate}>
+                  Generate
                 </Fab>
-                </div>
-                <Modal open={this.state.modalOpen}>
-                    <div>
-                        <Typography variant='h3'>
-                            {`Congrats: you've been presented with ${this.state.selected}`}
-                        </Typography>
-                    </div>
-                </Modal>
-            </div>
+              </Grid>
+              <Modal open={this.state.modalOpen}>
+                  <div>
+                      <Typography variant='h3'>
+                          {`Congrats: you've been presented with ${this.state.selected}`}
+                      </Typography>
+                  </div>
+              </Modal>
+            </Grid>
         );
     }
 }
+
+export const OptionsFormComponent = withStyles(styles)(OptionsForm);
